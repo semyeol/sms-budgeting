44b600c8e36e22d8e8727ac3e166005b7cafe997
sem <stchoi1018@gmail.com>
2025-09-13 23:04:50 -0700

    lil setup

---
+++ src/config/categories.ts
@@ -0,0 +1,26 @@
+export const INCOME_CATEGORIES = [
+    'Work',
+    'Other'
+]
+
+export const SAVINGS_CATEGORIES = [
+    'Savings',
+    'Investing'
+]
+
+export const BILLS_CATEGORIES = [
+    'Parents',
+    'Wifi',
+    'Gym',
+    'Subscription',
+    'School'
+]
+
+export const EXPENSE_CATEGORIES = [
+    'Food & Dining',
+    'Shopping',
+    'Activity',
+    'Grocery',
+    'Gas',
+    'Other',
+]
